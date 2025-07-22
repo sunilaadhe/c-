ab61ae0bf7ff2f01684101afc41e2733d7d120f9
sunilaadhe <sunilaadhe62@gmail.com>
2025-07-22 22:18:53 +0530

    first

---
+++ sunil.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter a number :";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+
+    if(a>b){
+        cout<<"a is greater then b or c";
+    }
+    else if(b>a){
+        cout<<"b is greater then a or c";
+    }
+    else if(c>a){
+        cout<<"c is greater then a or b";
+    }
+
+    else{
+                cout<<"c is greater then a or b";
+
+    }
+    return 0;
+}                                                          

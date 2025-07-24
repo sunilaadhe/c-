3593a78b0e2c0bca4e7f566d66c2d308d306e85b
sunilaadhe <sunilaadhe62@gmail.com>
2025-07-24 21:49:55 +0530

    second commit

--- sunil.cpp
+++ sunil.cpp
@@ -1,25 +1,33 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cout<<"enter a number :";
+//     cin>>a;
+//     cin>>b;
+//     cin>>c;
+
+//     if(a>b){
+//         cout<<"a is greater then b or c";
+//     }
+//     else if(b>a){
+//         cout<<"b is greater then a or c";
+//     }
+//     else if(c>a){
+//         cout<<"c is greater then a or b";
+//     }
+
+//     return 0;
+// }                                                          
+#include<iostream>
 using namespace std;
 int main(){
-    int a,b,c;
-    cout<<"enter a number :";
+    int a;
+    cout<<"enter number :";
     cin>>a;
-    cin>>b;
-    cin>>c;
 
-    if(a>b){
-        cout<<"a is greater then b or c";
+    for(int i=0;i<=a;i++){
+        
+        cout<<i;
     }
-    else if(b>a){
-        cout<<"b is greater then a or c";
-    }
-    else if(c>a){
-        cout<<"c is greater then a or b";
-    }
-
-    else{
-                cout<<"c is greater then a or b";
-
-    }
-    return 0;
-}                                                          
+}
